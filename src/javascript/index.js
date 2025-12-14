c75cf154326169378d6ebfd348448cb9cf4720f3
t_coding <trapperwetzel@gmail.com>
2025-12-13 21:20:23 -0600

    readfile javascript

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,11 +1,5 @@
 import * as fs from "fs";
 
-const readFiles = () => {
-
-  const files = fs.readFileSync("../../data/test_data.txt", 'utf-8');
-  console.log(files);
-
-
-}
-readFiles();
+const files = fs.readFileSync("../../data/test_data.txt", 'utf-8');
+console.log(files);
 
