0f15e0efa6007938ec2efb8ce0b53c60fdd955f2
t_coding <trapperwetzel@gmail.com>
2025-12-24 21:58:46 -0600

    csharp read

---
+++ src/csharp/FileReader/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+using System.IO;
+
+public class FileReader {
+
+  public string readFile(string file) {
+    string contents = File.ReadAllText(file);
+    return contents; 
+  }
+
+}
+public class Program
+{
+  static void Main(string[] args){
+    FileReader reader = new FileReader();
+    string file = reader.readFile("../../../data/test_data.txt");
+    Console.WriteLine(file);
+  }
+
+}
